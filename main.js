672f0cc831889152161d07581096242d4d07d4dc
Yonle <yonle@lecturify.net>
2024-08-21 19:52:59 +0700

    .

---
+++ main.js
@@ -0,0 +1,49 @@
+const t = document.getElementById("t")
+const img = document.getElementById("merahputih")
+
+let curText = ""
+let int = null
+let tim = null
+
+function tulis(text, c) {
+  clearInterval(int)
+  clearTimeout(tim)
+  t.style.visibility = "visible";
+  text = text.split("")
+  curText = "";
+  t.innerText = curText
+  
+  int = setInterval(() => {
+    let p = text.shift();
+    if (!p) {
+      clearInterval(int)
+      clearTimeout(tim)
+      tim = setTimeout(_ => {
+        t.style.visibility = "hidden";
+        if (typeof(c) === "function") c()
+      }, 2000)
+      return
+    }
+    curText += p;
+    t.innerText = curText;
+  }, 70)
+}
+
+let respected = false
+
+function awalan() {
+  if (respected) return
+  respected = true;
+  tulis("Hiduplah Indonesia Raya", _ => {
+    let op = 0.5
+    let int = setInterval(() => {
+      if (op < 0.1) {
+        clearInterval(int)
+        tulis("Merdeka", _ => location.href = "about:blank")
+        return
+      }
+      op -= 0.1
+      img.style.opacity = op
+    }, 120)
+  })
+}
